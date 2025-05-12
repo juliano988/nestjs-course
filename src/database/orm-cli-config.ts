4646f47f9959200126a2b872d7b6d17f58fd2000
Júlio Faria <julio-pinheiroo@hotmail.com>
2025-05-12 19:20:24 -0300

    feat: adiciona suporte à configuração de ambiente com @nestjs/config e refatora conexão com o banco de dados

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,5 +1,6 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1746798732622 } from 'src/migrations/1746798732622-CreateCoursesTable';
 import { CreateTagsTable1746811964429 } from 'src/migrations/1746811964429-CreateTagsTable';
 import { CreateCoursesTagsTable1746813453546 } from 'src/migrations/1746813453546-CreateCoursesTagsTable';
@@ -7,6 +8,19 @@ import { AddCoursesIdToCoursesTagsTable1746814427774 } from 'src/migrations/1746
 import { AddTagsIdToCoursesTagsTable1746816156928 } from 'src/migrations/1746816156928-AddTagsIdToCoursesTagsTable';
 import { UpdateRelationTableName1747073158993 } from 'src/migrations/1747073158993-UpdateRelationTableName';
 import { UpdateTagTableName1747073840810 } from 'src/migrations/1747073840810-UpdateTagTableName';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
